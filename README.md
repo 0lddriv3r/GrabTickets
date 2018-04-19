7be52051fb26223813524fc8db9da11ba492bab0
JiajieZhuo <jiajiezhuo@163.com>
2018-04-19 20:38:02 +0800

    Init commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Grab Tickets
+
+大麦网抢票Chrome插件
+
+## 技术
+Chrome开发者工具加载js插件，心跳刷新页面进行DOM元素选择。
+
+## 用法
+
+1. 修改main.js中的`performid priceid person`字段，分别为演唱会id、价格id、人数id。（可以通过Chrome监听工具查看）
+2. 用Chrome扩展工具的开发者模式加载已解压的扩展工具，工具栏显示加载表示成功。
+3. 打开大麦网插件，选择要听的演唱会子页面即可。
---
+++ main.js
@@ -0,0 +1,62 @@
+var performid = '9047306';
+var priceid = '12252483';
+var person = '1';
+
+while (1) {
+  var re = main();
+
+  if (re == 1) {
+    alert('快去付款！')
+    break;
+  }
+
+  sleep(1000);
+}
+
+function sleep(n) {
+  var start = new Date().getTime();
+  while (true) if (new Date().getTime() - start > n) break;
+}
+
+function main() {
+  var buy = document.getElementById('btnBuyNow');
+  var Xuanzuo = document.getElementById('btnXuanzuo');
+  if (buy !== null) {
+    var Buytext = buy.innerHTML;
+    if (Buytext == '立即购买' || Buytext == '立即预定') {
+      grab_icket()
+      return 1;
+    } else {
+      document.location.reload()
+    }
+  } else if (Xuanzuo !== null) {
+    var Xuanzuotext = Xuanzuo.innerHTML;
+    if (Xuanzuotext == '选座购买') {
+      Xuanzuo.click()
+      return 1;
+    } else {
+      document.location.reload()
+    }
+  } else {
+    document.location.reload()
+  }
+}
+
+function grab_icket() {
+    var firstperform = document.getElementById('firstperform');
+    firstperform.className = 'itm  itm-sel';
+    var priceList = document.getElementById('priceList').childNodes;
+    var children = priceList[3].childNodes[1].childNodes;
+    console.log(children.length);
+    for (var i = 0; i < children.length; i++) {
+        if (children[i].getAttribute('data-priceid') == priceid) {
+            console.log(children[i]);
+            children[i].className = 'itm  itm-sel';
+        }
+    }
+
+    var str = '<div class="ct" style=""><ul class="lst"><li class="itm" style="" data-performid=' + performid + ' data-priceid=' + priceid + '><span class="txt txt-datetime ">"2018-05-02 周三 19:30"</span><span class="txt txt-price">"980"</span><span class="m-nums"><a class="btn btn-low" href="javascript:;">减</a><input class="ipt ipt-num" type="text" value=' + person + '><a class="btn btn-add" href="javascript:;">加</a></span><span class="tips tips-stock"><strong></strong></span><a class="btn btn-del" href="javascript:;"><i></i>删除</a></li></ul></div><div class="ops"><a href="javascript:;" class="u-btn u-btn-c1 u-btn-choose" id="btnXuanzuo" style="">选座购买</a><a class="u-btn u-btn-buynow" href="javascript:;" id="btnBuyNow">立即购买</a></div>';
+    document.getElementById('cartList').innerHTML = str;
+    var bt = document.getElementById('btnBuyNow');
+    bt.click();
+}
